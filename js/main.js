9f2f311fb4d62435ae9a7501433f42b5670e084c
Web-Designer-kanna <developerkanna045@gmail.com>
2023-12-06 09:03:56 -0800

    sample e-commerce website created

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let navBtn=document.querySelector("#navBtn");
+let nav=document.querySelector("nav");
+let navpage=document.querySelector(".nav-page");
+
+navBtn.addEventListener("click",()=>{
+    navpage.classList.toggle("active");
+    nav.classList.toggle("active");
+});
+
+
+
+
+
+
+
+
+
+
